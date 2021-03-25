cc6d2bf78332edaa9f73a03270465b26bd6c3f38
MiquelCanal <miquelcanal98@gmail.com>
2021-03-25 17:50:14 +0100

    variable a created

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,4 @@
+int a;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -5,5 +6,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
