c99a9fec0b06d07918a6cae0d7de5583a931f94b
Jan Gomez Escoda <jangomezescoda@gmail.com>
2021-04-08 18:24:52 +0200

    y a mi que me cuentas

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#define MQ3pin A3
 
+float sensorValue;  //variable to store sensor value
+
+void setup() {
+  Serial.begin(9600); // sets the serial port to 9600
+  Serial.println("MQ3 warming up!");
+  //delay(20000); // allow the MQ3 to warm up
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(MQ3pin); // read analog input pin 0
 
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue);
+  
+  delay(200); // wait 2s for next reading
 }
